0a0b674ff82cb61ac1eac7ab4c127a080d74b621
Jordan Eckowitz <jordan.eckowitz@gmail.com>
2018-05-04 21:19:13 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,12 +2,8 @@ def my_select(array)
   if array.length > 0
     even = []
     even = array.select {|x| x.even?}
-    yield even 
+    return even 
   else
     puts "This block should not run!"
   end
-end
-
-my_select do |x|
-  return x 
 end
