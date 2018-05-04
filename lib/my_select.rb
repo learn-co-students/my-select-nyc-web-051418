e38a046c9d6f99115f57163cc0b3888e3d4c9977
Jordan Eckowitz <jordan.eckowitz@gmail.com>
2018-05-04 21:09:16 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if array.length > 0
+    even = []
+    even = array.select {|x| x.even?}
+    return even 
+  else
+    puts "This block should not run!"
+  end
 end
