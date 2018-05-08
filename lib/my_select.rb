752fba7ce7800b2fa421a261d41f4e5be3ee131b
Tony Ho <ho.tonym@gmail.com>
2018-05-08 00:33:12 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
 def my_select(collection)
- # your code here!
+
+    if collection.length <= 0
+      "This block should not run!"
+    else
+    counter = 0
+    new_array = Array.new
+      while counter < collection.length
+        if yield(collection[counter]) == true
+          new_array << (collection[counter])
+        end
+      counter +=1
+      end
+    end
+  return new_array
+
+=begin
+   collection.select do |number|
+     number.even?
+   end
+=end
+
 end
